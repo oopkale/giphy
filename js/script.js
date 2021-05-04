52f6d232b79ffa3924024d427f020e912f509657
Glitch (aileen-giphyprjct) <none>
2021-05-04 01:09:00 +0000

    🍧🏯 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -2,10 +2,20 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-
+let custom_api_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
 $(".search-button").click(function(){
-  
+  fetch(custom_api_url)
+
+        .then(function(response) {
+        	return response.json();
+        })
+
+        .then(function(data) {
+        console.log(data);
+       	 	
+        });
+});
 
   
-});
+
 
