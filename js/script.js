6d380f52f656c26a4d33f78b7b1f41b6a7d100cf
Glitch (aileen-giphyprjct) <none>
2021-05-18 01:03:10 +0000

    🐴🍘 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-let custom_api_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
+let custom_api_url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC";
 $(".search-button").click(function(){
   fetch(custom_api_url)
 
